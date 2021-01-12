e026c7aa30ab6a3f75c0dd7b487837bd93b35cb8
stevencheng-smartdesign <steven.cheng@smartdesign.com.tw>
2021-01-12 22:32:28 +0800

    add package & babel

---
+++ shopping_cart/cart.test.ts
@@ -0,0 +1,49 @@
+import {Cart} from "./cart";
+
+describe('Case', function () {
+    let blackCat, hsinChu, postOffice: string;
+    let cart: Cart;
+    let shippingFee: number;
+    beforeEach(() => {
+        blackCat = 'black cat';
+        hsinChu = 'hsin chu';
+        postOffice = 'post office';
+        cart = new Cart();
+    });
+
+    function feeShouldBe(expected: number, actualFee: number) {
+        expect(actualFee).toEqual(expected);
+    }
+
+    it('test black cat with light weight', function () {
+        shippingFee = cart.shippingFee(blackCat, 30, 20, 10, 5);
+        feeShouldBe(150, shippingFee);
+    });
+
+    it('test black cat with heavy weight', function () {
+        shippingFee = cart.shippingFee(blackCat, 30, 20, 10, 50);
+        feeShouldBe(500, shippingFee);
+    });
+
+    it('test hsin chu with small size', function () {
+        shippingFee = cart.shippingFee(hsinChu, 30, 20, 10, 50);
+        feeShouldBe(144, shippingFee);
+    });
+
+    it('test hsin chu with huge size', function () {
+        shippingFee = cart.shippingFee(hsinChu, 100, 20, 10, 50);
+        feeShouldBe(480, shippingFee);
+    });
+
+    it('test post office by weight', function () {
+        shippingFee = cart.shippingFee(postOffice, 100, 20, 10, 3);
+        feeShouldBe(110, shippingFee);
+    });
+
+    it('test post office by size', function () {
+        shippingFee = cart.shippingFee(postOffice, 100, 20, 10, 300);
+        feeShouldBe(440, shippingFee);
+    });
+
+
+});
---
+++ shopping_cart/cart.ts
@@ -0,0 +1,29 @@
+export class Cart {
+
+    shippingFee(shipper: string, length: number, width: number, height: number, weight: number): number {
+        if (shipper === "black cat") {
+            if (weight > 20) {
+                return 500;
+            } else {
+                return 100 + weight * 10;
+            }
+        } else if (shipper === "hsin chu") {
+            let size: number;
+            size = length * width * height;
+            if (length > 100 || width > 100 || height > 100) {
+                return size * 0.00002 * 1100 + 500;
+            } else {
+                return size * 0.00002 * 1200;
+            }
+        } else if (shipper === "post office") {
+            let feeByWeight, size, feeBySize: number;
+            feeByWeight = 80 + weight * 10;
+            size = length * width * height;
+            feeBySize = size * 0.00002 * 1100;
+            return feeByWeight < feeBySize ? feeByWeight : feeBySize;
+        } else {
+            throw new Error("shipper not exist");
+
+        }
+    }
+}
