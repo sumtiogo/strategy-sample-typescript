c30a219231541805ce29f6d15b2433f91c182475
Ayson Chang <sumtiogo@gmail.com>
2021-10-09 10:01:17 +0800

    refactor: reduce magic string by enum
    1. 用 constructor 語法糖簡化 class code
    2. 用 enum 減少 magic string，並且確保 shipperFeeDictionary 不會漏寫

--- shopping_cart/cart.ts
+++ shopping_cart/cart.ts
@@ -1,73 +1,56 @@
 export class Product {
-    private readonly _length;
-    private readonly _width;
-    private readonly _height;
-    private readonly _weight;
+  constructor(
+    public readonly length: number,
+    public readonly width: number,
+    public readonly height: number,
+    public readonly weight: number,
+  ) {}
 
-    get length() {
-        return this._length;
-    }
-
-    get width() {
-        return this._width;
-    }
-
-    get height() {
-        return this._height;
-    }
-
-    get weight() {
-        return this._weight;
-    }
-
-    constructor(length: number, width: number, height: number, weight: number) {
-        this._length = length;
-        this._width = width;
-        this._height = height;
-        this._weight = weight;
-    }
-
-    size(product: Product) {
-        return product.length * product.width * product.height;
-    }
-
+  public get size() {
+    return this.length * this.width * this.height;
+  }
 }
 
+enum Shipper {
+  BlackCat = 'black cat',
+  HsinChu = 'hsin chu',
+  PostOffice = 'post office',
+}
+type CalFeeFunc = (p: Product) => number;
+
 export class Cart {
+  private static readonly shipperFeeDictionary: Record<Shipper, CalFeeFunc> = {
+    [Shipper.BlackCat]: Cart.calculateFeeByBlackCat,
+    [Shipper.HsinChu]: Cart.calculateFeeByHsinChu,
+    [Shipper.PostOffice]: Cart.calculateFeeByPostOffice,
+  };
 
-    private readonly shipperFeeDictionary = {
-        "black cat": Cart.calculateFeeByBlackCat,
-        "hsin chu": Cart.calculateFeeByHsinChu,
-        "post office": Cart.calculateFeeByPostOffice
+  static shippingFee(shipper: Shipper, product: Product): number {
+    if (shipper in Cart.shipperFeeDictionary) {
+      return Cart.shipperFeeDictionary[shipper](product);
     }
+    throw new Error('shipper not exist');
+  }
 
-    shippingFee(shipper: string, product: Product): number {
-        if (shipper in this.shipperFeeDictionary) {
-            return this.shipperFeeDictionary[shipper](product);
-        }
-        throw new Error("shipper not exist");
-    }
+  private static calculateFeeByPostOffice(product: Product) {
+    const feeByWeight = 80 + product.weight * 10;
+    const feeBySize: number = product.size * 0.00002 * 1100;
+    return Math.min(feeBySize, feeByWeight);
+  }
 
-    private static calculateFeeByPostOffice(product: Product) {
-        let feeByWeight, feeBySize: number;
-        feeByWeight = 80 + product.weight * 10;
-        feeBySize = product.size(product) * 0.00002 * 1100;
-        return Math.min(feeBySize, feeByWeight);
+  private static calculateFeeByHsinChu(product: Product) {
+    if (product.length > 100 || product.width > 100 || product.height > 100) {
+      return product.size * 0.00002 * 1100 + 500;
+    } else {
+      return product.size * 0.00002 * 1200;
     }
+  }
 
-    private static calculateFeeByHsinChu(product: Product) {
-        if (product.length > 100 || product.width > 100 || product.height > 100) {
-            return product.size(product) * 0.00002 * 1100 + 500;
-        } else {
-            return product.size(product) * 0.00002 * 1200;
-        }
+  private static calculateFeeByBlackCat(product: Product) {
+    if (product.weight > 20) {
+      return 500;
+    } else {
+      return 100 + product.weight * 10;
     }
-
-    private static calculateFeeByBlackCat(product: Product) {
-        if (product.weight > 20) {
-            return 500;
-        } else {
-            return 100 + product.weight * 10;
-        }
-    }
+  }
 }
